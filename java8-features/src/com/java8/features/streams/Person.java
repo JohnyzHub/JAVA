dbeecac86ae4b3025e0e027aa8c545a6fd82e4b6
JohnyzHub <jbshaik82@gmail.com>
2018-04-21 13:04:37 -0400

    Java8:Features:POJO

--- java8-features/src/com/java8/features/streams/Person.java
+++ java8-features/src/com/java8/features/streams/Person.java
@@ -4,7 +4,9 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Person {
 
@@ -17,6 +19,7 @@ public class Person {
 	private Gender gender;
 	private LocalDate dob;
 	private double income;
+	private Set<String> languages;
 
 	public Person(long id, String name, Gender gender, LocalDate dob, double income) {
 		super();
@@ -25,6 +28,7 @@ public class Person {
 		this.gender = gender;
 		this.dob = dob;
 		this.income = income;
+		languages = new HashSet<String>(0);
 	}
 
 	public long getId() {
@@ -79,21 +83,35 @@ public class Person {
 		this.income = income;
 	}
 
+	public void addLanguage(String language) {
+		this.languages.add(language);
+	}
+
+	public Set<String> getLanguages() {
+		return this.languages;
+	}
+
 	@Override
 	public String toString() {
-		return "\nPerson [id=" + id + ", name=" + name + ", gender=" + gender + ", dob=" + dob + ", income=" + income
-				+ "]";
+		return "Person [id=" + id + ", name=" + name + ", gender=" + gender + ", dob=" + dob + ", income=" + income
+				+ ", languages=" + languages + "]";
 	}
 
 	public static List<Person> persons() {
 		List<Person> persons = new ArrayList<Person>(5);
 		Person ken = new Person(1, "Ken", Gender.MALE, LocalDate.of(1982, Month.JULY, 10), 6000.00);
-		Person ben = new Person(1, "ben", Gender.MALE, LocalDate.of(1980, Month.AUGUST, 20), 2000.00);
-		Person sen = new Person(1, "sen", Gender.FEMALE, LocalDate.of(1986, Month.JULY, 15), 5000.00);
-		Person pen = new Person(1, "pen", Gender.MALE, LocalDate.of(1981, Month.JANUARY, 05), 7000.00);
-		Person fen = new Person(1, "fen", Gender.FEMALE, LocalDate.of(1984, Month.SEPTEMBER, 22), 6300.00);
-		persons.addAll(Arrays.asList(ken, ben, sen, pen, fen));
+		ken.addLanguage("java");
+		Person ben = new Person(2, "Ben", Gender.MALE, LocalDate.of(1980, Month.AUGUST, 20), 2000.00);
+		ben.addLanguage("scala");
+		Person sen = new Person(3, "Sen", Gender.MALE, LocalDate.of(1986, Month.JULY, 15), 5000.00);
+		sen.addLanguage("python");
+		Person pen = new Person(4, "Pen", Gender.FEMALE, LocalDate.of(1981, Month.JANUARY, 05), 7000.00);
+		pen.addLanguage("kotlin");
+		Person fen = new Person(5, "Fen", Gender.FEMALE, LocalDate.of(1984, Month.SEPTEMBER, 22), 6300.00);
+		fen.addLanguage("android");
+		Person zen = new Person(6, "Zen", Gender.FEMALE, LocalDate.of(1984, Month.SEPTEMBER, 12), 5300.00);
+		fen.addLanguage("Ruby");
+		persons.addAll(Arrays.asList(ken, ben, sen, pen, fen, zen));
 		return persons;
 	}
-
 }
