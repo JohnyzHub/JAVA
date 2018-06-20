6a8f6442f271e784e436eb1c0a0b036f32e10f7e
JohnyzHub <jbshaik82@gmail.com>
2018-06-20 15:25:06 -0400

    Design Pattern: Observer
    	Implementation of Observer pattern

---
+++ Core-Java/src/com/corejava/design/patterns/observer/FoxNews.java
@@ -0,0 +1,38 @@
+package com.corejava.design.patterns.observer;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+/**
+ * @author johnybasha
+ *
+ */
+public class FoxNews implements PropertyChangeListener {
+
+	private String news;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.beans.PropertyChangeListener#propertyChange(java.beans.
+	 * PropertyChangeEvent)
+	 */
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		String propertyName = evt.getPropertyName();
+		if (propertyName != null && propertyName.equalsIgnoreCase("politics")) {
+			this.news = (String) evt.getNewValue();
+		}
+	}
+
+	public void dispalyNews() {
+		System.out.println("Politics:: " + this.news);
+	}
+
+	@Override
+	public String toString() {
+		return "FoxNews";
+	}
+	
+	
+}
---
+++ Core-Java/src/com/corejava/design/patterns/observer/NewsAgency.java
@@ -0,0 +1,52 @@
+package com.corejava.design.patterns.observer;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.List;
+
+/**
+ * @author johnybasha
+ *
+ */
+public class NewsAgency {
+
+	private String weatherNews;
+	private String politicalNews;
+
+	private PropertyChangeSupport propertySupport;
+
+	public NewsAgency() {
+		propertySupport = new PropertyChangeSupport(this);
+	}
+
+	public void addPropertyChangeListeners(List<PropertyChangeListener> listeners) {
+		if (listeners == null || listeners.isEmpty()) {
+			return;
+		}
+		listeners.stream().forEach(a -> addPropertyChangeListener(a));
+	}
+
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		if (listener != null) {
+			propertySupport.addPropertyChangeListener(listener);
+			System.out.println("Added:"+listener);
+		}
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+		if (listener != null) {
+			propertySupport.removePropertyChangeListener(listener);
+			System.out.println("Removed:"+listener);
+		}
+	}
+
+	public void setWeatherNews(String news) {
+		propertySupport.firePropertyChange("weather", this.weatherNews, news);
+		this.weatherNews = news;
+	}
+
+	public void setPoliticalNews(String news) {
+		propertySupport.firePropertyChange("politics", this.politicalNews, news);
+		this.politicalNews = politicalNews;
+	}
+}
