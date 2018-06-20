c3dbca6c2277962068c90ce818744d2786fcd093
JohnyzHub <jbshaik82@gmail.com>
2018-06-20 15:25:33 -0400

    Design Pattern: Singleton
    	Implementation of Singleton pattern

---
+++ Core-Java/src/com/corejava/design/patterns/singleton/InnerStaticClass.java
@@ -0,0 +1,35 @@
+package com.corejava.design.patterns.singleton;
+
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+/**
+ * @author johnybasha
+ *
+ */
+public final class InnerStaticClass implements Serializable {
+
+	private static final long serialVersionUID = 1l;
+
+	private static class InstanceHolder {
+		private static final InnerStaticClass _instance = new InnerStaticClass();
+	}
+
+	private InnerStaticClass() {
+		if (InstanceHolder._instance != null) {
+			throw new RuntimeException("Use InnerStaticClass.getInstance() instead.");
+		}
+	}
+
+	public static InnerStaticClass getInstance() {
+		return InstanceHolder._instance;
+	}
+
+	private Object readResolve() throws ObjectStreamException {
+		return InstanceHolder._instance;
+	}
+
+	private Object writeReplace() throws ObjectStreamException {
+		return InstanceHolder._instance;
+	}
+}
